f3fbb2092c976eb441599c44b03d71490078ee87
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-11-09 09:02:46 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Talker } from './Talker';
+
+ReactDOM.render(
+    <Talker />
+  ,document.getElementById('app')
+);
+
